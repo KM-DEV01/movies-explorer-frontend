e31cf2542941fa021b9d3f8317dd4416c0fbdc01
unknown <kamil.mardanov04@yandex.ru>
2023-07-08 15:07:11 +0300

    fix: readme update

--- src/components/shared/Input/Input.jsx
+++ src/components/shared/Input/Input.jsx
@@ -5,7 +5,7 @@ function Input({ inputName = '', errorText = '', inputType = 'text' }) {
   return (
     <div className="input">
       <span className="input__caption">{inputName}</span>
-      <input className={`input__field ${errorText && 'input__field_error'}`} type={inputType} />
+      <input className={`input__field ${errorText && 'input__field_error'}`} type={inputType} required />
       <span className="input__caption input__caption_error">{errorText}</span>
     </div>
   );
